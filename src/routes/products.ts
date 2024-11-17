150e9a7a8dbea6f1013099379628e16ab3a00819
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-11-17 16:19:06 -0300

    feat: Validacao produto existe, nova rota atualizarProduto

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -18,14 +18,26 @@ export async function productsRoutes(app: FastifyInstance) {
             request.body
         )
 
+        const productAlreadyExists = await prisma.products.findFirst({
+            where: {
+                name: {
+                    contains: name,
+                    mode: 'insensitive',
+                }
+            }
+        })
 
-        const products = await prisma.products.create({data: {
+        if(productAlreadyExists){
+            return reply.status(409).send({ error: "Produto já existente no sistema", product: productAlreadyExists })
+        }
+
+        await prisma.products.create({data: {
             name,
             price,
             img_link: img_link || "",
         }})
 
-        return reply.status(201).send(JSON.stringify(products))
+        return reply.status(201).send({message:"Produto criado com sucesso"})
     })
 
     app.get('/allProducts', async(request, reply) => {
@@ -97,7 +109,6 @@ export async function productsRoutes(app: FastifyInstance) {
 
             const productsExamples = await fetch('https://fakestoreapi.com/products')
             const data = await productsExamples.json();
-            console.log(data)
 
             data.forEach(async (element: Product) => {
 
@@ -116,5 +127,53 @@ export async function productsRoutes(app: FastifyInstance) {
             console.error('Error na requisição.', error)
         }
         
+    })
+
+    app.put('/update/:id', async(request, reply) => {
+
+        const updateProductBodySchema = z.object({
+            name: z.string().nullable(),
+            price: z.number().nullable(),
+            img_link: z.string().url(),
+
+        })
+
+        const productIdParams = z.object({
+            id: z.string()
+        })
+
+        const { name, price, img_link } = updateProductBodySchema.parse(request.body)
+
+        const { id } = productIdParams.parse(request.params);
+        
+        try {
+
+            const product = await prisma.products.findFirst({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            if(!product) {
+                return reply.status(404).send({message: "Produto não encontrado 😢."})
+            }
+
+            if(name !== null) product.name = name;
+            if(price !== null) product.price = price;
+            if(img_link !== null) product.img_link = img_link;
+
+            await prisma.products.update({
+                where: { id },
+                data: {
+                     name: product.name,
+                     price: product.price,
+                     img_link: product.img_link,
+                }
+            })
+        } catch (error) {
+            console.error(error);
+            reply.status(500).send({messagem: 'Error ao atualizar o produto'})
+        }
+
     })
 }
