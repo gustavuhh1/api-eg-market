9a64492b00e3bf2411b86f67cfcafda06e08cefb
GustavoMartinsR <gustavomrodrigues11@gmail.com>
2024-11-20 22:18:57 -0300

    aparenta estar funfando

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 import { productsRoutes } from './routes/products';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 export const app = fastify()
 
@@ -14,3 +21,11 @@ app.register(productsRoutes, {
     prefix: "products",
 });
 
+app.register(fastifyStatic, {
+    root: path.join(__dirname, "public"),
+    prefix: '/',
+})
+
+app.get('/', (req, reply) => {
+    reply.sendFile('index.html')
+})
